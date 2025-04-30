39caa969f2193c7775a665d63998e46a0f238afa
Shashikant Pandey <pandeyshashikant1434@gmail.com>
2025-05-01 01:10:08 +0530

    Updated more

--- To_Do/script.js
+++ To_Do/script.js
@@ -2,7 +2,8 @@ const todoForm =document.querySelector("form");
 const todoInput=document.getElementById("todo-input");
 const todoListUl=document.getElementById("todo-list");
 
-let allTodos =[];
+let allTodos =getTodos();
+updateTodoList();
 
 todoForm.addEventListener("submit",function(e){
     e.preventDefault();
@@ -13,18 +14,19 @@ function addTodo(){
     if(todoText.length>0){
         allTodos.push(todoText);
         updateTodoList();
+        saveTodos();
         todoInput.value="";
-    }
+    }}
 function updateTodoList(){
-    todoListUl.innerHTML=" ";
+    todoListUl.innerHTML="";
     allTodos.forEach((todo,todoIndex)=>{
         todoItem=createTodoItem(todo,todoIndex);
         todoListUl.append(todoItem);
     })
-}
-    
 
 }
+
+
 function createTodoItem(todo,todoIndex){
     const todoLi = document.createElement("li");
     let todoId="todo-"+todoIndex;
@@ -36,6 +38,25 @@ function createTodoItem(todo,todoIndex){
           <button class="delete-button">
             <i class="material-icons">delete</i>
           </button>
-    `
+    ` 
+    const deleteButton = todoLi.querySelector(".delete-button");
+    deleteButton.addEventListener("click",()=>{
+        deleteTodoItem(todoIndex)
+    })
     return todoLi;
+}
+
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_,i)=>i !==todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+
+function saveTodos(){
+    const todoJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos",todoJson);
+}
+function getTodos(){
+    const todos = localStorage.getItem("todos") ||"[]";
+    return JSON.parse(todos);
 }
